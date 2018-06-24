a3cb48eb68761f322339b6a0717fd3ad0d9cc74c
ktr0731 <aizu.s1230022@gmail.com>
2018-06-24 19:46:39 +0900

    create one transport per one API request

--- lib/grpcweb/transport.go
+++ lib/grpcweb/transport.go
@@ -1,17 +1,55 @@
 package grpcweb
 
-type TransportBuilder func(*Client) Transport
+import (
+	"bytes"
+	"fmt"
+	"net/http"
 
-var DefaultTransportBuilder TransportBuilder = func(_ *Client) Transport {
-	return &HTTPTransport{}
-}
+	"github.com/pkg/errors"
+)
+
+type TransportBuilder func(host string, req *Request) Transport
+
+var DefaultTransportBuilder TransportBuilder = HTTPTransportBuilder
 
 // Transport creates new request.
 // Transport is created only one per one request, MUST not use used transport again.
 type Transport interface {
-	Send()
+	Send(body []byte) error
 }
 
 type HTTPTransport struct {
-	Transport
+	sent bool
+
+	host   string
+	req    *Request
+	client *http.Client
+}
+
+func (t *HTTPTransport) Send(body []byte) error {
+	if t.sent {
+		return errors.New("Send must be called only one time per one Request")
+	}
+	defer func() {
+		t.sent = true
+	}()
+
+	req, err := http.NewRequest(http.MethodPost, t.req.URL(t.host), bytes.NewReader(body))
+	if err != nil {
+		return errors.Wrap(err, "failed to build the API request")
+	}
+	res, err := t.client.Do(req)
+	if err != nil {
+		return errors.Wrap(err, "failed to send the API")
+	}
+	fmt.Println(res)
+	return nil
+}
+
+func HTTPTransportBuilder(host string, req *Request) Transport {
+	return &HTTPTransport{
+		host:   host,
+		req:    req,
+		client: &http.Client{},
+	}
 }
