69e496c8a438ee316a1926d2c6f6db7f05fa1df0
ktr0731 <aizu.s1230022@gmail.com>
2018-06-26 19:08:32 +0900

    use own gRPC Web server in testing

--- lib/grpcweb/client_test.go
+++ lib/grpcweb/client_test.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/golang/protobuf/protoc-gen-go/descriptor"
 	"github.com/jhump/protoreflect/desc"
-	"github.com/k0kubun/pp"
-	"github.com/ktr0731/grpc-test/api"
+	"github.com/jhump/protoreflect/dynamic"
+	"github.com/ktr0731/grpc-test/server"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
@@ -21,7 +21,7 @@ type protoHelper struct {
 	*desc.FileDescriptor
 
 	s map[string]*descriptor.ServiceDescriptorProto
-	m map[string]*descriptor.DescriptorProto
+	m map[string]*dynamic.Message
 }
 
 func (h *protoHelper) getServiceByName(t *testing.T, n string) *descriptor.ServiceDescriptorProto {
@@ -38,11 +38,11 @@ func (h *protoHelper) getServiceByName(t *testing.T, n string) *descriptor.Servi
 	return svc
 }
 
-func (h *protoHelper) getMessageTypeByName(t *testing.T, n string) *descriptor.DescriptorProto {
+func (h *protoHelper) getMessageTypeByName(t *testing.T, n string) *dynamic.Message {
 	if h.m == nil {
-		h.m = map[string]*descriptor.DescriptorProto{}
+		h.m = map[string]*dynamic.Message{}
 		for _, msg := range h.GetMessageTypes() {
-			h.m[msg.GetName()] = msg.AsDescriptorProto()
+			h.m[msg.GetName()] = dynamic.NewMessage(msg)
 		}
 	}
 	msg, ok := h.m[n]
@@ -85,12 +85,12 @@ func TestClient(t *testing.T) {
 	service := pkg.getServiceByName(t, "Example")
 
 	t.Run("NewClient returns new API client", func(t *testing.T) {
-		client := NewClient("http://localhost:50051", WithTransportBuilder(stubTransportBuilder))
+		client := NewClient("http://localhost:50052", WithTransportBuilder(stubTransportBuilder))
 		assert.NotNil(t, client)
 	})
 
 	t.Run("Send an unary API", func(t *testing.T) {
-		client := NewClient("http://localhost:50051", WithTransportBuilder(stubTransportBuilder))
+		client := NewClient("http://localhost:50052", WithTransportBuilder(stubTransportBuilder))
 
 		in, out := pkg.getMessageTypeByName(t, "SimpleRequest"), pkg.getMessageTypeByName(t, "SimpleResponse")
 		req, err := NewRequest(service, service.GetMethod()[0], in, out)
@@ -100,36 +100,20 @@ func TestClient(t *testing.T) {
 	})
 
 	t.Run("real", func(t *testing.T) {
+		defer server.New().Serve(nil, true).Stop()
+
 		client := NewClient("http://localhost:50051")
 
-		// in, out := pkg.getMessageTypeByName(t, "SimpleRequest"), pkg.getMessageTypeByName(t, "SimpleResponse")
-		in := &api.SimpleRequest{Name: "ktr"}
-		out := &api.SimpleResponse{}
+		in := pkg.getMessageTypeByName(t, "SimpleRequest")
+		in.SetFieldByName("name", "ktr")
+
+		out := pkg.getMessageTypeByName(t, "SimpleResponse")
+
 		req, err := NewRequest(service, service.GetMethod()[0], in, out)
 		assert.NoError(t, err)
 		err = client.Send(context.Background(), req)
 		assert.NoError(t, err)
 
-		// b, err := proto.Marshal(out)
-		// require.NoError(t, err)
-		// pp.Println(string(b))
-		pp.Println(out)
+		assert.Equal(t, "hello, ktr", out.GetFieldByName("message"))
 	})
-
-	// t.Run("real", func(t *testing.T) {
-	// 	client := NewClient("http://localhost:50051")
-	//
-	// 	// in, out := pkg.getMessageTypeByName(t, "SimpleRequest"), pkg.getMessageTypeByName(t, "SimpleResponse")
-	// 	in := &api.UnaryMessageRequest{Name: &api.Name{FirstName: "foo", LastName: "bar"}}
-	// 	out := &api.SimpleResponse{}
-	// 	req, err := NewRequest(service, service.GetMethod()[1], in, out)
-	// 	assert.NoError(t, err)
-	// 	err = client.Send(context.Background(), req)
-	// 	assert.NoError(t, err)
-	//
-	// 	// b, err := proto.Marshal(out)
-	// 	// require.NoError(t, err)
-	// 	// pp.Println(string(b))
-	// 	pp.Println(out)
-	// })
 }
